db0062340ca1997be74253cbdcc0462ff35e1abc
truongdt3 <truongdt3@msb.com.vn>
2023-06-05 14:49:06 +0700

    update

--- main.py
+++ main.py
@@ -7,20 +7,42 @@ import os
 
 OWNER = 'truongdt3'
 
+template_conf = '''
+- intermediate_host: 10.0.4.124
+  intermediate_port: 22
+  ssh_username: truongdt3
+  ssh_password: example
+  remote_bind_address: 
+    host: 10.221.180.139
+    port: 22
+  local_bind_address:
+    host: 127.0.0.1
+    port: 2225
+- intermediate_host: 127.0.0.1
+  intermediate_port: 2225
+  ssh_username: ubuntu
+  ssh_pkey: analytics-prod.pem
+  remote_bind_address: 
+    host: 54.251.202.212
+    port: 22
+  local_bind_address:
+    host: 127.0.0.1
+    port: 2233
+'''
+
 class CustomNamespace(Namespace):
     owner: str
+    working_dir: str
 
 def parse_arguments() -> CustomNamespace:
     parser = ArgumentParser()
     parser.add_argument('--owner', required=True, type=str, help='program owner')
+    parser.add_argument('--working-dir', required=False, default='.', type=str, help='Working dir')
     return parser.parse_args(namespace=CustomNamespace)
 
-app_path = os.path.join(os.path.expanduser('~'), 'AppData/Local/GnourtNet')
-if not os.path.exists(app_path):
-    os.mkdir(app_path)
-
-logging.basicConfig(level=logging.ERROR, filename=os.path.join(app_path, 'logs.log'), filemode='a')
-logger = logging.getLogger(__name__)
+def get_logger():
+    logging.basicConfig(level=logging.WARNING, filename='logs.log', filemode='a')
+    return logging.getLogger(__name__)
 
 
 if __name__ == '__main__':
@@ -28,6 +50,12 @@ if __name__ == '__main__':
     if args.owner != OWNER:
         logger.error('Wrong owner')
         raise 'Wrong owner'
+    os.chdir(args.working_dir)
+    logger = get_logger()
+    if not os.path.exists('config.yaml'):
+        logger.warning('Config not found')
+        with open('config.yaml', 'w') as f:
+            f.write(template_conf)
     with open('config.yaml', 'r') as f:
         conf = yaml.safe_load(f)
     logger.info(f'Config: {conf}')
