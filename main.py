37077bc8ea4c2e244ce55721e450836ab6b9f817
truongdt3 <truongdt3@msb.com.vn>
2023-06-05 13:15:11 +0700

    .

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
 from sshtunnel import SSHTunnelForwarder, open_tunnel
 from time import sleep
-import paramiko
+import yaml
+import logging
+from argparse import ArgumentParser, Namespace
+import os
 
+OWNER = 'truongdt3'
 
-def port_fwd():
-    server = SSHTunnelForwarder(
-        '10.0.4.124',
-        ssh_username="truongdt3",
-        ssh_password="basmnjnszk465",
-        remote_bind_address=('127.0.0.1', 8097),
-        local_bind_address=('localhost', 8097)
-    )
-    server.start()
-    while True:
-        sleep(1)
+class CustomNamespace(Namespace):
+    owner: str
 
-def port_fwd_pro():
-    
-    with open_tunnel(
-        ('10.0.4.124', 22),
-        ssh_username="truongdt3",
-        ssh_password="basmnjnszk465",
-        remote_bind_address=('127.0.0.1', 22),
-        local_bind_address=('127.0.0.1', 10022)
-    ) as tunnel:
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect('127.0.0.1', 10022)
-        # do some operations with client session
-        client.close()
+def parse_arguments() -> CustomNamespace:
+    parser = ArgumentParser()
+    parser.add_argument('--owner', required=True, type=str, help='program owner')
+    return parser.parse_args(namespace=CustomNamespace)
 
-    # while True:
-    #     sleep(1)
+app_path = os.path.join(os.path.expanduser('~'), 'AppData/Local/GnourtNet')
+if not os.path.exists(app_path):
+    os.mkdir(app_path)
 
-    print('FINISH!')
+logging.basicConfig(level=logging.ERROR, filename=os.path.join(app_path, 'logs.log'), filemode='a')
+logger = logging.getLogger(__name__)
+
 
 if __name__ == '__main__':
-    port_fwd_pro()
+    args = parse_arguments()
+    if args.owner != OWNER:
+        logger.error('Wrong owner')
+        raise 'Wrong owner'
+    with open('config.yaml', 'r') as f:
+        conf = yaml.safe_load(f)
+    logger.info(f'Config: {conf}')
+    servers = []
+    for e in conf:
+        server = SSHTunnelForwarder(
+            (e['intermediate_host'], e['intermediate_port']),
+            ssh_username=e['ssh_username'],
+            ssh_password=e.get('ssh_password', None),
+            ssh_pkey=e.get('ssh_pkey', None),
+            remote_bind_address=(e['remote_bind_address']['host'], e['remote_bind_address']['port']),
+            local_bind_address=(e['local_bind_address']['host'], e['local_bind_address']['port'])
+        )
+        servers.append(server)
+    for server in servers:
+        server.start()
+    try:
+        while True:
+            sleep(1)
+    except Exception as ex:
+        logger.error(ex)
+        for server in servers:
+            server.close()
